906069795b7774ac55479e111ae0b04e2977be14
Arijit Mondal <arijitm717@gmail.com>
2026-08-01 11:35:56 +0530

    feat: add random-word-slugs dependency and implement project page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,24 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import { toast } from "sonner";
 
 export default function Page() {
   const [value, setValue] = useState("");
+  const router = useRouter();
 
   const trpc = useTRPC();
 
-  const { data: messages, isLoading } = useQuery(
-    trpc.messages.getMany.queryOptions(),
-  );
-
-  const createMessage = useMutation(
-    trpc.messages.create.mutationOptions({
-      onSuccess: () => {
-        toast.success("Background job started");
+  const createProject = useMutation(
+    trpc.projects.create.mutationOptions({
+      onSuccess: (data) => {
+        router.push(`/projects/${data.id}`);
+      },
+      onError: (err) => {
+        toast.error(err.message);
       },
     }),
   );
@@ -29,23 +30,13 @@ export default function Page() {
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
 
       <Button
-        disabled={createMessage.isPaused}
+        disabled={createProject.isPaused}
         onClick={() => {
-          createMessage.mutate({ prompt: value });
+          createProject.mutate({ prompt: value });
         }}
       >
         Code agent run in background
       </Button>
-
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <div>
-          {messages?.map((m) => (
-            <p key={m.id}>{JSON.stringify(m)}</p>
-          ))}
-        </div>
-      )}
     </div>
   );
 }
